0e14c7b8a113038d6de0bfb27a08c06672ab2155
Daniel <88162416+Danisaski@users.noreply.github.com>
2025-05-06 01:12:32 +0200

    Update 06_tmux_starship.sh

--- scripts/06_tmux_starship.sh
+++ scripts/06_tmux_starship.sh
@@ -12,3 +12,5 @@ ln -sfn "$SCRIPT_DIR/../configs/.tmux.conf" ~/.tmux.conf
 ln -sfn "$SCRIPT_DIR/../configs/starship.toml" ~/.config/starship.toml
 
 ln -sfn "$SCRIPT_DIR/../configs/config" ~/.config/ghostty/config
+
+ln -sfn "$SCRIPT_DIR/../configs/.alacritty.toml" ~/.alacritty.toml
