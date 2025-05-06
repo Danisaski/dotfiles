3e0fde25a76633819e0940271fa373fca82de9a5
Daniel <88162416+Danisaski@users.noreply.github.com>
2025-05-06 11:49:32 +0200

    Changed to jdk-openjdk

--- scripts/01_system_deps.sh
+++ scripts/01_system_deps.sh
@@ -5,4 +5,4 @@ echo "[*] Installing base packages..."
 sudo pacman -Syu --noconfirm
 
 sudo pacman -S --noconfirm base-devel git curl wget unzip zip sudo \
-    jdk17-openjdk fzf ripgrep lazygit zsh tmux neovim starship
+    jdk-openjdk fzf ripgrep lazygit zsh tmux neovim starship
